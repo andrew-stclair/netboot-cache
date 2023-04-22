1df3be0512b66c85e2c3ab17390e7964abe5b674
Andrew St Clair <andrew_stclair95@hotmail.com>
2023-04-22 05:21:03 +0000

    Add caching of latest netboot menu sig

--- cache.py
+++ cache.py
@@ -1,49 +1,56 @@
 import json
+import requests
 
-config = json.loads(open("cache.json").read())
+config = json.loads(open("cache.json", encoding="utf-8").read())
 
-nginx_config = f"# /etc/nginx/nginx.conf\n"
-nginx_config += f"\n"
-nginx_config += f"user nginx;\n"
-nginx_config += f"\n"
-nginx_config += f"# Set number of worker processes automatically based on number of CPU cores.\n"
-nginx_config += f"worker_processes auto;\n"
-nginx_config += f"\n"
-nginx_config += f"# Enables the use of JIT for regular expressions to speed-up their processing.\n"
-nginx_config += f"pcre_jit on;\n"
-nginx_config += f"\n"
-nginx_config += f"# Configures default error logger.\n"
-nginx_config += f"error_log /var/log/nginx/error.log warn;\n"
-nginx_config += f"\n"
-nginx_config += f"# Includes files with directives to load dynamic modules.\n"
-nginx_config += f"include /etc/nginx/modules/*.conf;\n"
-nginx_config += f"\n"
-nginx_config += f"# Include files with config snippets into the root context.\n"
-nginx_config += f"include /etc/nginx/conf.d/*.conf;\n"
-nginx_config += f"\n"
-nginx_config += f"events {{\n"
-nginx_config += f"        # The maximum number of simultaneous connections that can be opened by\n"
-nginx_config += f"        # a worker process.\n"
-nginx_config += f"        worker_connections 1024;\n"
-nginx_config += f"}}\n"
-nginx_config += f"\n"
-nginx_config += f"http {{\n"
-nginx_config += f"    proxy_cache_path /cache keys_zone=cache:{config['cache_zone_size']} max_size={config['cache_max_size']};\n"
-nginx_config += f"    server {{\n"
-nginx_config += f"        proxy_cache cache;\n"
+MENUVER = requests.get(
+    "https://api.github.com/repos/netbootxyz/netboot.xyz/releases/latest",
+    timeout=15).json()['tag_name']
+
+config['locations']['/sigs/'] = f"http://boot.netboot.xyz/{MENUVER}/sigs/"
+
+NGINX_CONFIG = "# /etc/nginx/nginx.conf\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "user nginx;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "# Set number of worker processes automatically based on number of CPU cores.\n"
+NGINX_CONFIG += "worker_processes auto;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "# Enables the use of JIT for regular expressions to speed-up their processing.\n"
+NGINX_CONFIG += "pcre_jit on;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "# Configures default error logger.\n"
+NGINX_CONFIG += "error_log /var/log/nginx/error.log warn;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "# Includes files with directives to load dynamic modules.\n"
+NGINX_CONFIG += "include /etc/nginx/modules/*.conf;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "# Include files with config snippets into the root context.\n"
+NGINX_CONFIG += "include /etc/nginx/conf.d/*.conf;\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "events {{\n"
+NGINX_CONFIG += "        # The maximum number of simultaneous connections that can be opened by\n"
+NGINX_CONFIG += "        # a worker process.\n"
+NGINX_CONFIG += "        worker_connections 1024;\n"
+NGINX_CONFIG += "}\n"
+NGINX_CONFIG += "\n"
+NGINX_CONFIG += "http {\n"
+NGINX_CONFIG += f"    proxy_cache_path /cache keys_zone=cache:{config['cache_zone_size']} max_size={config['cache_max_size']};\n"
+NGINX_CONFIG += "    server {\n"
+NGINX_CONFIG += "        proxy_cache cache;\n"
 
 for location in config['locations']:
-    nginx_config += f"        location {location} {{\n"
-    nginx_config += f"            proxy_pass {config['locations'][location]};\n"
-    nginx_config += f"            proxy_redirect off;\n"
-    nginx_config += f"            sub_filter \"{config['locations'][location]}\" \"{location}\";\n"
-    nginx_config += f"            sub_filter_once off;\n"
-    nginx_config += f"        }}\n"
+    NGINX_CONFIG += f"        location {location} {{\n"
+    NGINX_CONFIG += f"            proxy_pass {config['locations'][location]};\n"
+    NGINX_CONFIG += "            proxy_redirect off;\n"
+    NGINX_CONFIG += f"            sub_filter \"{config['locations'][location]}\" \"{location}\";\n"
+    NGINX_CONFIG += "            sub_filter_once off;\n"
+    NGINX_CONFIG += "        }\n"
 
-nginx_config += f"    }}\n"
-nginx_config += f"}}\n"
-nginx_config += f"\n"
+NGINX_CONFIG += "    }\n"
+NGINX_CONFIG += "}\n"
+NGINX_CONFIG += "\n"
 
-nginx_config_file = open("config/nginx.conf", "w")
-nginx_config_file.write(nginx_config)
-nginx_config_file.close()
+NGINX_CONFIG_FILE = open("config/nginx.conf", "w", encoding="utf-8")
+NGINX_CONFIG_FILE.write(NGINX_CONFIG)
+NGINX_CONFIG_FILE.close()
